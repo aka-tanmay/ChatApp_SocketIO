3109d9e10c43c9166ce67eb8cc9ea58feaf0831b
tanmay <tulsidas.sg2002@gmail.com>
2025-07-22 12:39:43 +0530

    designed whole frontend

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -35,7 +35,7 @@ const RightSidebar = ({selectedUser}) => {
         </div>
 
         <button className='absolute bottom-5 left-1/2 transform -translate-x-1/2
-        bg-gradient-to-r from-purple-400 to-voilet-600 text-white border-none
+        bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none
         text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
           Logout
         </button>

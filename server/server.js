f05e9db1c0c967bfda2dd89ef902c824013e4fdc
tanmay <tulsidas.sg2002@gmail.com>
2025-07-25 16:15:44 +0530

    Completed whole backend with socket.io

--- server/server.js
+++ server/server.js
@@ -2,16 +2,51 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messagesRoutes.js";
+import { Server } from "socket.io";
+import { log } from "console";
 
 // Create Express app and HTTP server
 const app =  express();
 const server = http.createServer(app) 
 
+// Initialize socket.io server
+export const io = new Server(server,{
+    cors: {origin: "*"}
+})
+
+// Store online users
+export const userSocketMap = {}; // { userId: socketId }
+
+// Socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit Online users to all connected  clients 
+    io.emit("geronlineUsers",object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(user))
+        
+    })
+    
+     
+})
+
 // Middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
 
+//Routes Setup
 app.use("/api/status",(req,res)=> res.send("Server is live "))
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter )
 
 
 

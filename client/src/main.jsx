f176e1676efc12b351a99cfe46fd6bf6a47952d0
tanmay <tulsidas.sg2002@gmail.com>
2025-07-29 16:40:16 +0530

    functions for chatcontainer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,19 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { AuthProvider } from '../context/AuthContext.jsx'
+import { ChartProvider } from '../context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider> 
+  <ChartProvider>
+
 
     <App />
 
-  </AuthProvider>
+    
+  </ChartProvider>
+   </AuthProvider>
   </BrowserRouter>,
 )
 
